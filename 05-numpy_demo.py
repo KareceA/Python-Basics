19e2f3489778163b6e9530bb359dff6d04cf686d
SalamiTech <salamigideon01@gmail.com>
2023-10-14 06:18:21 +0000

    added further instructions for library use case

--- 05-numpy_demo.py
+++ 05-numpy_demo.py
@@ -8,3 +8,17 @@ arr = np.array([1, 2, 3, 4, 5])
 
 # Calculate and print the mean of the array
 print("Mean:", np.mean(arr))
+
+
+
+
+
+
+# NumPy: A library for numerical computing in Python. You can demonstrate how to create arrays, perform arithmetic operations on them, and use built-in functions like mean, sum, etc.
+
+# Instructions:
+
+# 1. Install NumPy if you haven't already: pip install numpy
+# 2. Create a new Python file, e.g., numpy_demo.py.
+# 3. Copy and paste the above code into the file.
+# 4. Run the file: python numpy_demo.py
--- 06-pandas_demo.py
+++ 06-pandas_demo.py
@@ -11,3 +11,14 @@ df = pd.DataFrame(data)
 
 # Print the DataFrame
 print(df)
+
+
+# Pandas: A library for data manipulation and analysis. You can show how to create a DataFrame, read data from a CSV file, and perform basic operations like filtering and grouping.
+
+# Instructions:
+
+# 1. Install Pandas if you haven't already: pip install pandas
+# 2. Create a new Python file, e.g., pandas_demo.py.
+# 3. Copy and paste the above code into the file.
+# 4. Run the file: python pandas_demo.py
+

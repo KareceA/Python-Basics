d8cb112490f6caf6a81333a1146ff421e8e9aaae
SalamiTech <salamigideon01@gmail.com>
2023-10-14 05:10:31 +0000

    Added demonstration code for NumPy and Pandas

---
+++ 05-numpy_demo.py
@@ -0,0 +1,10 @@
+# To demonstrate the basic functionality of the NumPy library, specifically creating arrays and calculating the mean.
+
+# Import the NumPy library
+import numpy as np
+
+# Create a 1-dimensional array using NumPy
+arr = np.array([1, 2, 3, 4, 5])
+
+# Calculate and print the mean of the array
+print("Mean:", np.mean(arr))
---
+++ 06-pandas_demo.py
@@ -0,0 +1,13 @@
+# To demonstrate the basic functionality of the Pandas library, specifically creating a DataFrame.
+
+# Import the Pandas library
+import pandas as pd
+
+# Create a dictionary with sample data
+data = {'Name': ['Alice', 'Bob'], 'Age': [25, 30]}
+
+# Convert the dictionary to a Pandas DataFrame
+df = pd.DataFrame(data)
+
+# Print the DataFrame
+print(df)
